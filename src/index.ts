d2dbed88ed6db4df6b497144504a400bfe52e418
Kaustubh Deshpande <kaustubh285@gmail.com>
2026-02-24 21:42:06 +0000

    Revert "[minor] Flux dump/support multiline"

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { initFluxCommand, resetFluxCommand } from "./commands/init.command";
-import { brainDumpAddCommand, handleBrainDump } from "./commands/dump.command";
+import { brainDumpAddCommand } from "./commands/dump.command";
 import { searchBrainDumpCommand } from "./commands/search.command";
 import { configCommand } from "./commands/config.command";
 import { helpOption } from "./commands/flux.option";
 import packageJson from "../package.json"
 import { getFluxPath } from "./utils";
-
 const program = new Command()
 
 program.name(`flux`).description('Git-aware CLI context manager for ADHD developers').version(packageJson.version);
@@ -20,12 +19,9 @@ program.command('reset')
 	.description('Resets flux in the current repository')
 	.action(resetFluxCommand)
 
-program.command('dump [message...]')
-	.option('-m, --multiline', 'Enable multiline input mode')
-	.description('Add a brain dump with a message. Use --multiline for multi-line input.')
-	.action(async (message, options) => {
-		await handleBrainDump(message, options);
-	})
+program.command('dump <message...>')
+	.description('Add a brain dump with a message. You can also include tags by using #tag in the message.')
+	.action(brainDumpAddCommand)
 
 program.command('search [query...]')
 	.description('Search brain dumps with a query. If no query is provided, lists all brain dumps for the current month.')
@@ -37,5 +33,8 @@ program.command('config <fields...>')
 	.description('Update configuration fields. Example: flux config search.limit 10')
 	.action(configCommand)
 
+program.command("test").action(async () => {
+	console.log(`Path is - ${await getFluxPath()}`)
+})
 
 program.parse(process.argv);
